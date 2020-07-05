4b077ce490c44f6bf8a09101a34e393ea926a942
Diego Pereira <diegogomespereira@gmail.com>
2020-07-05 19:17:01 -0400

    Add meta tags for SEO

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,6 +9,14 @@ export default function Layout({ children }) {
     <Fragment>
       <Head>
         <title>GitHub Profile</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <meta
+          name="description"
+          content="An attempt to improve GitHub profile page"
+        />
         <link
           rel="icon"
           href="/favicon.ico"
